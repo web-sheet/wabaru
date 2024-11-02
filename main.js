bf46a59b7b0965b5464a1d321e2d19d399ea1128
web-sheet <websheetweb@gmail.com>
2024-11-02 11:23:14 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,22 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import { Client } from 'whatsapp-web.js';
-import qrcode from 'qrcode';
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.use(express.static('public')); 
-app.get('/api/qrcode', (req, res) => {
-    if (app.locals.qrCode) {
-        res.json({ qrCode: app.locals.qrCode });
-    } else {
-        res.status(404).json({ message: 'QR code not available yet.' });
-    }
-});
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
 const client = new Client();
-client.on('qr', async (qr) => {
-    try {
-        const qrCode = await qrcode.toDataURL(qr);
-        app.locals.qrCode = qrCode;  
-    } catch (error) {
-        console.error('Error generating QR code:', error);
-    }
-});
 
 client.on('ready', () => {
     console.log('Client is ready!');
 });
 
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('message_create', message => {
+	if (message.body === '!ping') {
+		// send back "pong" to the chat the message was sent in
+		client.sendMessage(message.from, 'pong');
+	}
+});
+
+
 client.initialize();
-export default app;
